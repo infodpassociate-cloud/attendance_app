58631d60a3fba1b6eb9771edfa317cf47bcd9350
infodpassociate-cloud <info.dpassociate@gmail.com>
2026-03-23 17:19:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // ✅ Yahan apna Apps Script Web App URL daalo
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzW_IDv8pqBtTDwTTsAQUnq0teX9q4n7gS_6pxLICDfzWM20aPX87xEG2kPWNiRS0oB/exec";
